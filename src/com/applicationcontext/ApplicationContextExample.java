ee4d6115dbbd62cf21feb5b318a5e4e1fc13ab6e
venkateswarluk <venkateswarlu.kakani@imaginea.com>
2019-01-15 01:23:49 +0530

    spring learning

--- src/com/applicationcontext/ApplicationContextExample.java
+++ src/com/applicationcontext/ApplicationContextExample.java
@@ -3,6 +3,7 @@ package com.applicationcontext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class ApplicationContextExample {
 
@@ -15,6 +16,11 @@ public class ApplicationContextExample {
 		ApplicationContext context1 = new AnnotationConfigApplicationContext(JavaBasedConfig.class);
 		SMSService smsService = (SMSService) context1.getBean("smsservice");
 		smsService.sendMessage();
+		
+		ApplicationContext context2 = new FileSystemXmlApplicationContext("E:/Practice/SpringLearning/src/application-context.xml");
+		EmailService emailService2 = (EmailService) context2.getBean("emailservice");
+		emailService2.sendMessage();
+		
 	      
 
 	}
